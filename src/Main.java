7506c9f83dfde9ca276998bbf3d61862689411bb
DJEDOU KODJO <tauny225@gmail.com>
2022-06-25 04:17:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double Price = 75;
+        double shippingCost = 0;
+        double totalPrice = 0;
+
+        if (Price >= 100)
+            {
+                System.out.println("The shipping cost is free, and the total price is:" + Price);
+            }
+        else
+            {
+                shippingCost = (Price * 2)/100;
+                totalPrice = shippingCost + Price;
+                System.out.println("The shipping cost is:" + shippingCost + " and the total price is:" + totalPrice);
+            }
+
+        // write your code here
+    }
+}
